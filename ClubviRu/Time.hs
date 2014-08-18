cbdc66ccc5bc6780bbde80db7bed7259ab92270d
Victor Gorokhov <me@rkit.pp.ru>
2014-08-18 10:11:52 +0400

    time handlers

---
+++ ClubviRu/Time.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
+module ClubviRu.Time where
+import SiteGen.Deps
+import ClubviRu.Resource
+import ClubviRu.Config.Site
+import Control.Monad.Trans
+import System.Directory
+import ClubviRu.Debug.Helpers
+import Data.Time
+
+runClubviTime :: (SiteConfig m, MonadIO m) => Time SP UTCTime m a -> m a
+runClubviTime = runTime cht cut
+  where
+    cht si = toFilePathM si >>= \ fp -> liftIO $ getModificationTime fp
+    cut = liftIO $ getCurrentTime
